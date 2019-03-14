16c7eeaae472e5d6cc8ff62bf1a3f6d26b121bc8
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2019-03-14 15:32:24 +0000

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { PrimeNgModule } from "./primeng.module";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, PrimeNgModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/primeng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { TabViewModule } from "primeng/tabview";
+
+@NgModule({
+  declarations: [],
+  imports: [TabViewModule],
+  exports: [TabViewModule],
+  providers: []
+})
+export class PrimeNgModule {}
